d2e813dd2cbbdcd881d4ca80da67a9e759210a60
lil2kki <142544489+thaLILNIKKI@users.noreply.github.com>
2026-05-11 15:09:12 +0400

    Add multiple scripts for game enhancements

---
+++ --load-all.lua
@@ -0,0 +1,25 @@
+-- TEA-TIME WALTZ (OLD)
+loadstring(game:HttpGet(     -- FOR CREAM
+    "https://raw.githubusercontent.com/thaLILNIKKI/my-outcome-memories/HEAD/tea-time-waltz-old.lua"
+))()
+-- Vasalto - The Sol Still Burns V3 (April Fools)
+loadstring(game:HttpGet(     -- FOR BLAZE
+    "https://raw.githubusercontent.com/thaLILNIKKI/my-outcome-memories/HEAD/sol-still-burns-v3.lua"
+))()
+-- additional handling functionality for player input so you feel it like shooter
+loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/thaLILNIKKI/my-outcome-memories/HEAD/normal-cream-rebind.lua"
+))()
+-- Bring back sounds that was removed due to Roblox moderation.
+loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/thaLILNIKKI/my-outcome-memories/HEAD/tripwire-voice.lua"
+))()
+-- Amy Rose Stun Voice Lines Fanmade.
+-- Sounds from: https://www.tiktok.com/@mewmasterss/video/7634645656992320799
+loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/thaLILNIKKI/my-outcome-memories/HEAD/amy-voice.lua"
+))()
+-- Hit V to toggle. EXPEREMENTAL SHIT REALLY
+loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/thaLILNIKKI/my-outcome-memories/HEAD/first-person.lua"
+))()
