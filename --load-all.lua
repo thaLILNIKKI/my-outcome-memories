040477651ac17d9a5a89d547e2cbc5b1e7a3a67b
lil2kki <142544489+thaLILNIKKI@users.noreply.github.com>
2026-05-11 15:18:10 +0400

    Refactor loadstring calls for script compatibility
    Updated loadstring calls to conditionally load scripts based on the OLDER_CREAM_LMS variable, ensuring compatibility with different versions.

--- --load-all.lua
+++ --load-all.lua
@@ -1,25 +1,13 @@
--- TEA-TIME WALTZ (OLD)
-loadstring(game:HttpGet(     -- FOR CREAM
-    "https://raw.githubusercontent.com/thaLILNIKKI/my-outcome-memories/HEAD/tea-time-waltz-old.lua"
-))()
+-- TEA-TIME WALTZ FOR CREAM
+if _g.OLDER_CREAM_LMS then loadstring(game:HttpGet("https://raw.githubusercontent.com/thaLILNIKKI/my-outcome-memories/HEAD/tea-time-waltz-old.lua"))()
+else loadstring(game:HttpGet("https://raw.githubusercontent.com/thaLILNIKKI/my-outcome-memories/HEAD/tea-time-waltz.lua"))() end
 -- Vasalto - The Sol Still Burns V3 (April Fools)
-loadstring(game:HttpGet(     -- FOR BLAZE
-    "https://raw.githubusercontent.com/thaLILNIKKI/my-outcome-memories/HEAD/sol-still-burns-v3.lua"
-))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/thaLILNIKKI/my-outcome-memories/HEAD/sol-still-burns-v3.lua"))()
 -- additional handling functionality for player input so you feel it like shooter
-loadstring(game:HttpGet(
-    "https://raw.githubusercontent.com/thaLILNIKKI/my-outcome-memories/HEAD/normal-cream-rebind.lua"
-))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/thaLILNIKKI/my-outcome-memories/HEAD/normal-cream-rebind.lua"))()
 -- Bring back sounds that was removed due to Roblox moderation.
-loadstring(game:HttpGet(
-    "https://raw.githubusercontent.com/thaLILNIKKI/my-outcome-memories/HEAD/tripwire-voice.lua"
-))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/thaLILNIKKI/my-outcome-memories/HEAD/tripwire-voice.lua"))()
 -- Amy Rose Stun Voice Lines Fanmade.
--- Sounds from: https://www.tiktok.com/@mewmasterss/video/7634645656992320799
-loadstring(game:HttpGet(
-    "https://raw.githubusercontent.com/thaLILNIKKI/my-outcome-memories/HEAD/amy-voice.lua"
-))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/thaLILNIKKI/my-outcome-memories/HEAD/amy-voice.lua"))()
 -- Hit V to toggle. EXPEREMENTAL SHIT REALLY
-loadstring(game:HttpGet(
-    "https://raw.githubusercontent.com/thaLILNIKKI/my-outcome-memories/HEAD/first-person.lua"
-))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/thaLILNIKKI/my-outcome-memories/HEAD/first-person.lua"))()
