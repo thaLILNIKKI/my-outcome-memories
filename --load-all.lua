a9185b175d393308d09b5fc86c1bb6dbad07c1c9
lil2kki <142544489+thaLILNIKKI@users.noreply.github.com>
2026-06-13 12:03:50 +0400

    Modify loadstring calls in --load-all.lua
    Updated loadstring calls to include additional functionality and commented out a previous loadstring for tripwire voice.

--- --load-all.lua
+++ --load-all.lua
@@ -1,15 +1,21 @@
 -- TEA-TIME WALTZ FOR CREAM
 if _G.OLDER_CREAM_LMS then loadstring(game:HttpGet("https://raw.githubusercontent.com/thaLILNIKKI/my-outcome-memories/HEAD/tea-time-waltz-old.lua"))()
 else loadstring(game:HttpGet("https://raw.githubusercontent.com/thaLILNIKKI/my-outcome-memories/HEAD/tea-time-waltz.lua"))() end
+
 -- Vasalto - The Sol Still Burns V3 (April Fools)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/thaLILNIKKI/my-outcome-memories/HEAD/sol-still-burns-v3.lua"))()
+
 -- additional handling functionality for player input so you feel it like shooter
--- loadstring(game:HttpGet("https://raw.githubusercontent.com/thaLILNIKKI/my-outcome-memories/HEAD/normal-cream-rebind.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/thaLILNIKKI/my-outcome-memories/HEAD/normal-cream-rebind.lua"))()
+
 -- Bring back sounds that was removed due to Roblox moderation.
-loadstring(game:HttpGet("https://raw.githubusercontent.com/thaLILNIKKI/my-outcome-memories/HEAD/tripwire-voice.lua"))()
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/thaLILNIKKI/my-outcome-memories/HEAD/tripwire-voice.lua"))()
+
 -- Amy Rose Stun Voice Lines Fanmade.
 loadstring(game:HttpGet("https://raw.githubusercontent.com/thaLILNIKKI/my-outcome-memories/HEAD/amy-voice.lua"))()
+
 -- Cream hurt voice lines from uneasy rest (94445064593939)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/thaLILNIKKI/my-outcome-memories/HEAD/cream-screams.lua"))()
+
 -- juno, ThatGuyNamedPanther, Astranova, feat. Johnny Gioeli - So, DON T BLINK -- FOR SONIC
 loadstring(game:HttpGet("https://raw.githubusercontent.com/thaLILNIKKI/my-outcome-memories/HEAD/so-dont-blink.lua"))()
